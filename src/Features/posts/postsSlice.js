affe1908d5e5106581d0fdf3b5288348d1e31124
patricktunez123 <tunezepatrick@gmail.com>
2022-02-08 14:01:32 +0200

    done

--- src/Features/posts/postsSlice.js
+++ src/Features/posts/postsSlice.js
@@ -1,9 +1,17 @@
-import { createSlice } from "@reduxjs/toolkit";
+import { createSlice, createAsyncThunk } from "@reduxjs/toolkit";
+import axios from "../../common/apiCall";
 
 const initialState = {
   loading: false,
   posts: [],
 };
+
+export const fetchPosts = createAsyncThunk("posts/fetchPosts", async () => {
+  const posts = await axios
+    .get("/posts")
+    .catch((error) => console.log("Error", error));
+  return posts;
+});
 
 const postsSlice = createSlice({
   name: "posts",
@@ -11,6 +19,27 @@ const postsSlice = createSlice({
   reducers: {
     getPosts: (state, { payload }) => {
       state.posts = payload;
+    },
+  },
+  extraReducers: {
+    [fetchPosts.pending]: (state) => {
+      return {
+        ...state,
+        loading: true,
+      };
+    },
+    [fetchPosts.fulfilled]: (state, { payload }) => {
+      return {
+        ...state,
+        loading: false,
+        posts: payload.data,
+      };
+    },
+    [fetchPosts.rejected]: (state) => {
+      return {
+        ...state,
+        loading: false,
+      };
     },
   },
 });
--- src/components/List/List.js
+++ src/components/List/List.js
@@ -1,31 +1,25 @@
 import React, { useEffect } from "react";
 import { useDispatch, useSelector } from "react-redux";
-import { getPosts } from "../../Features/posts/postsSlice";
-import axios from "../../common/apiCall";
+import { fetchPosts } from "../../Features/posts/postsSlice";
 
 const List = () => {
   const dispatch = useDispatch();
-  const { posts } = useSelector((state) => state.posts);
+  const { posts, loading } = useSelector((state) => state.posts);
 
   useEffect(() => {
-    const fetchPosts = async () => {
-      const posts = await axios
-        .get("/posts")
-        .catch((error) => console.log("There was an error:", error));
-      dispatch(getPosts(posts.data));
-    };
-    fetchPosts();
+    dispatch(fetchPosts());
   }, [dispatch]);
 
-  console.log("=====>>>", posts);
   return (
     <div>
-      {posts.map((post) => (
-        <>
-          <h5>{post.title}</h5>
-          <p>{post.body}</p>
-        </>
-      ))}
+      {loading
+        ? "loading..."
+        : posts?.map((post) => (
+            <div key={post.id}>
+              <h5>{post.title}</h5>
+              <p>{post.body}</p>
+            </div>
+          ))}
     </div>
   );
 };
